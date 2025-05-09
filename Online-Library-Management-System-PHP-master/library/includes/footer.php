fb7f92b07b61e3287c73f67597f7b7a3cd546236
Sakshi123rgb <sakshi129988@gmail.com>
2025-05-09 18:32:48 +0530

    Add files via upload

---
+++ Online-Library-Management-System-PHP-master/library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Online Library Management System |<a href="https://github.com/kumarpandule2000" target="_blank"  > Designed by : Saloni & Rakshita</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
